b69015319ca71ae0f284b5ec0fad571e7b4a908f
Samvel Khalatyan <sn.khalatyan@gmail.com>
2025-09-15 16:04:14 -0500

    Add demo C++ soluiton to test bazel

---
+++ iq/twopointer/twosum/solution.cc
@@ -0,0 +1,8 @@
+#include <optional>
+#include <utility>
+#include <vector>
+
+std::optional<std::pair<int, int>> Find(
+    [[maybe_unused]] const std::vector<int>& nn, [[maybe_unused]] int x) {
+  return std::nullopt;
+}
