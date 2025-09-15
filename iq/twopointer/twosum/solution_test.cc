40457fc3c7031830d77f9dd5014e6f0bd8d3d10d
Samvel Khalatyan <sn.khalatyan@gmail.com>
2025-09-15 16:04:14 -0500

    Add demo C++ soluiton to test bazel

---
+++ iq/twopointer/twosum/solution_test.cc
@@ -0,0 +1,13 @@
+// Copyright 2025 Samvel Khalatyan. All rights reserved.
+
+#include "iq/twopointer/twosum/solution.h"
+
+#include "gtest/gtest.h"
+
+namespace iq::twopointer::twosum {
+namespace {
+
+TEST(FindTest, Demo) {}
+
+}  // namespace
+}  // namespace iq::twopointer::twosum
