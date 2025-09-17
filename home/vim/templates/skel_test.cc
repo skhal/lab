fd21965beae862bf35c7d07f9e4a58ab9f334f2c
Samvel Khalatyan <sn.khalatyan@gmail.com>
2025-09-17 18:39:38 -0500

    Add c++ test vim skeleton

---
+++ home/vim/templates/skel_test.cc
@@ -0,0 +1,11 @@
+// Copyright @YEAR@ Samvel Khalatyan. All rights reserved.
+
+#include "@HEADER@"
+
+#include "gtest/gtest.h"
+
+namespace @NAMESPACE@ {
+namespace {
+
+}  // namespace
+}  // namespace @NAMESPACE@
