f33617d76b6348e864639144da8b97534d18b238
Samvel Khalatyan <sn.khalatyan@gmail.com>
2025-09-19 20:25:30 -0500

    Add example test for twopointer/palindrome

---
+++ iq/twopointer/palindrome/solution_example_test.go
@@ -0,0 +1,15 @@
+// Copyright 2025 Samvel Khalatyan. All rights reserved.
+
+package palindrome_test
+
+import (
+	"fmt"
+
+	"github.com/skhal/lab/iq/twopointer/palindrome"
+)
+
+func Example() {
+	fmt.Println(palindrome.Is("Don't nod"))
+	// Output:
+	// true
+}
