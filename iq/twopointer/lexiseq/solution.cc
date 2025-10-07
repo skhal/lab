5b92fd59c7ff3ddaeb902b13ab9f8b2068609440
Samvel Khalatyan <sn.khalatyan@gmail.com>
2025-10-07 10:39:07 -0500

    Add c++ solution to lexicographical sequence iq.

---
+++ iq/twopointer/lexiseq/solution.cc
@@ -0,0 +1,77 @@
+// Copyright 2025 Samvel Khalatyan. All rights reserved.
+
+#include <algorithm>
+#include <iterator>
+#include <string>
+#include <string_view>
+#include <tuple>
+#include <utility>
+
+#include "iq/twopointer/lexiseq/solution.h"
+
+namespace iq::twopointer::lexiseq {
+namespace {
+
+constexpr int kSizeMin = 2;
+
+struct Pivot {
+  char ch;
+  int idx;
+
+  friend bool operator==(const Pivot& a, const Pivot& b) {
+    return std::tie(a.ch, a.idx) == std::tie(b.ch, b.idx);
+  }
+};
+
+constexpr Pivot kPivotInvalid = {.ch = -1, .idx = -1};
+
+// findPivot scans the string s backwards from the end until it finds the first
+// non-ascending character, called pivot. It returns the found character and
+// its index, or kPivotInvalid if the search fails.
+Pivot findPivot(std::string_view s) {
+  for (int i = s.size() - 1; i > 0; --i) {
+    if (const int p = i - 1; s[p] < s[i]) {
+      return {.ch = s[p], .idx = p};
+    }
+  }
+  return kPivotInvalid;
+}
+
+// findPivotNext scans the string s bakwards from the end until it finds the
+// first character that is larger than the pivot. It returns the position of
+// the character or zero if the search fails.
+int findPivotNext(std::string_view s, char pivot) {
+  for (int i = s.size() - 1; i >= 0; --i) {
+    if (s[i] > pivot) {
+      return i;
+    }
+  }
+  return 0;
+}
+
+}  // namespace
+
+std::string Next(std::string_view s) {
+  std::string buf{s};
+  if (s.size() < kSizeMin) {
+    return buf;
+  }
+  const Pivot pivot = findPivot(buf);
+  if (pivot == kPivotInvalid) {
+    std::reverse(buf.begin(), buf.end());
+    return buf;
+  }
+  const int pivot_next_idx =
+      pivot.idx + 1 + findPivotNext(s.substr(pivot.idx + 1), pivot.ch);
+  std::swap(buf[pivot.idx], buf[pivot_next_idx]);
+  std::reverse(
+      [&buf, &pivot]() {
+        auto it = buf.begin();
+        std::advance(it, pivot.idx + 1);
+        return it;
+      }(),
+      buf.end());
+  return buf;
+}
+
+}  // namespace iq::twopointer::lexiseq
---
+++ iq/twopointer/lexiseq/solution_test.cc
@@ -0,0 +1,63 @@
+// Copyright 2025 Samvel Khalatyan. All rights reserved.
+
+#include <cctype>
+#include <iomanip>
+#include <ostream>
+#include <vector>
+
+#include "gtest/gtest.h"
+#include "iq/twopointer/lexiseq/solution.h"
+
+namespace iq::twopointer::lexiseq {
+namespace {
+
+using ::testing::TestParamInfo;
+using ::testing::TestWithParam;
+using ::testing::ValuesIn;
+
+constexpr char kCharUnderscore = '_';
+
+struct NextTestParam {
+  std::string name;
+  std::string s;
+  std::string want;
+
+  friend std::ostream& operator<<(std::ostream& os, const NextTestParam& tp) {
+    return os << std::quoted(tp.name);
+  }
+};
+
+class NextTest : public TestWithParam<NextTestParam> {};
+
+TEST_P(NextTest, Test) {
+  const NextTestParam& tp = GetParam();
+
+  const std::string got = Next(tp.s);
+
+  EXPECT_EQ(got, tp.want);
+}
+
+const std::vector<NextTestParam> kTestParams{
+    {.name = "empty", .s = "", .want = ""},
+    {.name = "one letter", .s = "a", .want = "a"},
+    {.name = "two letters same", .s = "aa", .want = "aa"},
+    {.name = "two letters ascending", .s = "ab", .want = "ba"},
+    {.name = "two letters descending", .s = "ba", .want = "ab"},
+    {.name = "three letters same", .s = "aaa", .want = "aaa"},
+    {.name = "three letters first two same", .s = "aab", .want = "aba"},
+    {.name = "three letters first and last same", .s = "aba", .want = "baa"},
+    {.name = "three letters second and last same", .s = "baa", .want = "aab"},
+};
+
+INSTANTIATE_TEST_SUITE_P(NextTest, NextTest, ValuesIn(kTestParams),
+                         [](const TestParamInfo<NextTestParam>& info) {
+                           std::string name = info.param.name;
+                           std::replace_if(
+                               name.begin(), name.end(),
+                               [](char c) { return !std::isalnum(c); },
+                               kCharUnderscore);
+                           return name;
+                         });
+
+}  // namespace
+}  // namespace iq::twopointer::lexiseq
