4daca19f8cf48b035eaf5e24416bf94b890de188
Samvel Khalatyan <sn.khalatyan@gmail.com>
2025-10-07 12:31:14 -0500

    Add c++ solution to palindrome iq

---
+++ iq/twopointer/palindrome/solution.cc
@@ -0,0 +1,33 @@
+// Copyright 2025 Samvel Khalatyan. All rights reserved.
+
+#include <cctype>
+#include <string_view>
+
+#include "iq/twopointer/palindrome/solution.h"
+
+namespace iq::twopointer::palindrome {
+namespace {
+
+constexpr int kSizeMin = 1;
+
+}  // namespace
+
+bool Is(std::string_view s) {
+  if (s.size() < kSizeMin) {
+    return true;
+  }
+  for (int i = 0, j = s.size() - 1; i < j; ++i, --j) {
+    while (i < j && !std::isalnum(s[i])) {
+      ++i;
+    }
+    while (i < j && !std::isalnum(s[j])) {
+      --j;
+    }
+    if (i < j && s[i] != s[j]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+}  // namespace iq::twopointer::palindrome
---
+++ iq/twopointer/palindrome/solution_test.cc
@@ -0,0 +1,67 @@
+// Copyright 2025 Samvel Khalatyan. All rights reserved.
+
+#include <algorithm>
+#include <cctype>
+#include <iomanip>
+#include <ostream>
+#include <string>
+
+#include "gtest/gtest.h"
+#include "iq/twopointer/palindrome/solution.h"
+
+namespace iq::twopointer::palindrome {
+namespace {
+
+using ::testing::TestParamInfo;
+using ::testing::TestWithParam;
+using ::testing::ValuesIn;
+
+constexpr char kCharUnderscore = '_';
+
+struct IsTestParam {
+  std::string name;
+  std::string s;
+  bool want;
+
+  friend std::ostream& operator<<(std::ostream& os, const IsTestParam& tp) {
+    return os << std::quoted(tp.name) << " s: " << std::quoted(tp.s);
+  }
+};
+
+class IsTest : public TestWithParam<IsTestParam> {};
+
+TEST_P(IsTest, pass) {
+  const IsTestParam& tp = GetParam();
+
+  const bool got = Is(tp.s);
+
+  ASSERT_EQ(got, tp.want);
+}
+
+const IsTestParam kIsTestParams[] {
+    {.name = "empty", .s = "", .want = true},
+    {.name = "one letter", .s = "a", .want = true},
+    {.name = "one digit", .s = "1", .want = true},
+    {.name = "one non-alphanumeric letter", .s = ".", .want = true},
+    {.name = "two letters", .s = "aa", .want = true},
+    {.name = "two digits", .s = "11", .want = true},
+    {.name = "two distinct digits", .s = "12", .want = false},
+    {.name = "letter and digit", .s = "a1", .want = false},
+    {.name = "letter and non-alphanumeric", .s = "a.", .want = true},
+    {.name = "digit and non-alphanumeric", .s = "1.", .want = true},
+    {.name = "non-alphanumeric and letter", .s = ".a", .want = true},
+    {.name = "non-alphanumeric and digit", .s = ".1", .want = true},
+};
+
+INSTANTIATE_TEST_SUITE_P(IsTest, IsTest, ValuesIn(kIsTestParams),
+                         [](const TestParamInfo<IsTestParam>& info) {
+                           std::string name = info.param.name;
+                           std::replace_if(
+                               name.begin(), name.end(),
+                               [](char c) { return !std::isalnum(c); },
+                               kCharUnderscore);
+                           return name;
+                         });
+
+}  // namespace
+}  // namespace iq::twopointer::palindrome
