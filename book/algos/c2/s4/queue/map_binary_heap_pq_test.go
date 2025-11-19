0c759fba4567a23e503745ad31eaba0891ab5c0e
Samvel Khalatyan <sn.khalatyan@gmail.com>
2025-11-19 09:35:44 -0600

    Remove nnn from MapBinaryHeapPQ example

--- book/algos/c2/s4/queue/map_binary_heap_pq_test.go
+++ book/algos/c2/s4/queue/map_binary_heap_pq_test.go
@@ -15,17 +15,17 @@ import (
 func ExampleMapBinaryHeapPQ() {
 	// Multiway merge combines N sorted arrays using a priority queue to pick up
 	// the next array with min element.
-	nnn := [][]int{
-		{1, 5, 7, 9},
-		{2, 6},
-		{3, 4, 8},
-	}
+	//
 	// The PQ stores (item, slice-tail).
 	pq := queue.NewMapBinaryHeapPQ[int, []int](func(x, y int) bool {
 		// Emultate MinPQ to pick up the next smallest item
 		return lessInt(y, x)
 	})
-	for _, nn := range nnn {
+	for _, nn := range [][]int{
+		{1, 5, 7, 9},
+		{2, 6},
+		{3, 4, 8},
+	} {
 		pq.Push(nn[0], nn[1:])
 	}
 	var sorted []int
