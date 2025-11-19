6b88bfc0c9b49e69e25face4dbdc6cbedb9bbee8
Samvel Khalatyan <sn.khalatyan@gmail.com>
2025-11-19 14:16:27 -0600

    Ignore c++ code in iq/21 Go code

--- iq/21/anagram/find_all_test.cc
+++ iq/21/anagram/find_all_test.cc
@@ -2,6 +2,9 @@
 //
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+//
+// clang-format off-next-line
+//go:build ignore
 
 #include "iq/21/anagram/find_all.h"
 
