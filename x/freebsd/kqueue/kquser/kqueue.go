ab4283088ad0daaac5d49989decb6e78aac1635f
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-01-05 16:38:19 -0600

    Demo send/receive of a user event with kqueue
    NO_ISSUE

---
+++ x/freebsd/kqueue/kquser/kqueue.go
@@ -0,0 +1,125 @@
+// Copyright 2026 Samvel Khalatyan. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"syscall"
+	"time"
+)
+
+// KQueue wraps kqueue(2) descriptor.
+type KQueue struct {
+	fd int
+}
+
+// NewKQueue initializes a kqueue in the Kernel. Kqueue is stateful. Make sure
+// to clean up Kernel resources when done with the queue.
+func NewKQueue() (*KQueue, error) {
+	fd, err := syscall.Kqueue()
+	if err != nil {
+		return nil, err
+	}
+	return &KQueue{
+		fd: fd,
+	}, nil
+}
+
+// Close releases Kernel resources associated with this instance of kqueue(2).
+func (kq *KQueue) Close() error {
+	if err := syscall.Close(kq.fd); err != nil {
+		return err
+	}
+	kq.fd = 0
+	return nil
+}
+
+// Register configures kqueue(2) to monitor user events with a given EventID.
+func (kq *KQueue) Register(eid EventID) error {
+	changes := []syscall.Kevent_t{
+		{
+			Ident:  uint64(eid),
+			Filter: syscall.EVFILT_USER,
+			Flags:  syscall.EV_ADD | syscall.EV_CLEAR, // event flags
+			// user events may have any value set in the lower 24-bits of fflags.
+			// kqueue(2) propagates these to the receiver.
+			Fflags: syscall.NOTE_FFNOP, // ignore input fflags
+		},
+	}
+	events := []syscall.Kevent_t(nil) // do not poll events
+	var timeout syscall.Timespec      // zero-value, return immediately
+	if n, err := syscall.Kevent(kq.fd, changes, events, &timeout); err != nil {
+		return err
+	} else if n != 0 {
+		return fmt.Errorf("unexpected %d pending events for user identifier: %d", n, eid)
+	}
+	return nil
+}
+
+type EventID uint64
+
+const (
+	_ EventID = iota
+	EventIDOne
+	EventIDTwo
+)
+
+func (eid EventID) String() string {
+	switch eid {
+	default:
+		return fmt.Sprintf("event-id-%d", eid)
+	case EventIDOne:
+		return "event-id-one"
+	case EventIDTwo:
+		return "event-id-two"
+	}
+}
+
+// Send triggers a user event with EventID.  It returns an error if kevent(2)
+// fails.
+func (kq *KQueue) Send(eid EventID) error {
+	changes := []syscall.Kevent_t{
+		{
+			Ident:  uint64(eid),          // user event can have any uint64-id
+			Filter: syscall.EVFILT_USER,  // it will be a user event
+			Fflags: syscall.NOTE_TRIGGER, // send the event
+		},
+	}
+	events := []syscall.Kevent_t(nil) // do not get events
+	timeout := syscall.NsecToTimespec((10 * time.Millisecond).Nanoseconds())
+	if n, err := syscall.Kevent(kq.fd, changes, events, &timeout); err != nil {
+		return err
+	} else if n != 0 {
+		return fmt.Errorf("unexpected %d pending events in send user id: %d", n, eid)
+	}
+	return nil
+}
+
+// Poll check whether kqueue(2) has a user event available. It is a blocking
+// call. Zero duration returns immediately.
+func (kq *KQueue) Poll(timeout time.Duration) (*UserEvent, bool) {
+	changes := []syscall.Kevent_t(nil)                    // do not change kqueue
+	events := make([]syscall.Kevent_t, 1)                 // get at most 1 event
+	tout := syscall.NsecToTimespec(timeout.Nanoseconds()) // 0 return immediately
+	if n, err := syscall.Kevent(kq.fd, changes, events, &tout); err != nil {
+		panic(err)
+	} else if n == 0 {
+		return nil, false
+	}
+	return &UserEvent{
+		ID: EventID(events[0].Ident),
+	}, true
+}
+
+// UserEvent wraps the event identifier.
+type UserEvent struct {
+	ID EventID
+}
+
+// String implements fmt.Stringer interface.
+func (evt *UserEvent) String() string {
+	return fmt.Sprintf("%s", evt.ID)
+}
---
+++ x/freebsd/kqueue/kquser/main.go
@@ -0,0 +1,120 @@
+// Copyright 2026 Samvel Khalatyan. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+User demonstrates kqueue(2) send and receive a user event.
+
+Synopsis:
+
+	kquser
+
+Background:
+
+BSD-like systems (*BSD, MacOS, etc.) provide kqueue(2) to monitor events from a
+wide range of systems: file system, processes, signals, jails, timer, user, etc.
+(MacOS does not include jails).
+
+For the record, Linux has similar mechanism, implemented via select(2), poll(2),
+epoll(7), or inotify(7).
+
+Kqueue(2) is a stateful mechanism: Kernel keeps track of subscriptions. It is
+important to close kqueue(2) when done to clean up resources.
+
+Kqueue(2) provides a single function to manage and poll events. It guarantees to
+process manage-events first, and then update the poll-events. Even though it is
+possible to use both kinds of events in the same call to kevent(2), it is best
+to separate the two.
+
+Example:
+
+This example demonstrates the following aspects of working with kqueue(2):
+
+- Create and cleanup kqueue(2) Kernel resources.
+- Subscribe to User events.
+- Send and receive user events.
+
+Output:
+
+	2026/01/05 16:13:50 sent event event-id-two
+	2026/01/05 16:13:50 received event event-id-two
+*/
+package main
+
+import (
+	"errors"
+	"log"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func main() {
+	kq, err := NewKQueue()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer kq.Close()
+	if err := run(kq); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run(kq *KQueue) error {
+	events := []EventID{EventIDOne, EventIDTwo}
+	if err := register(kq, events); err != nil {
+		return err
+	}
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func(kq *KQueue, eid EventID) {
+		defer wg.Done()
+		send(kq, eid)
+	}(kq, MustGetRandomItem(events))
+	wg.Add(1)
+	go func(kq *KQueue) {
+		defer wg.Done()
+		receive(kq)
+	}(kq)
+	wg.Wait()
+	return nil
+}
+
+func register(kq *KQueue, events []EventID) error {
+	for _, eid := range events {
+		if err := kq.Register(eid); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func send(kq *KQueue, eid EventID) {
+	if err := kq.Send(eid); err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println("sent event", eid)
+}
+
+func receive(kq *KQueue) {
+	// Poll at most N times to prevent deadlock if send fails. Avoid context in demo.
+	const maxPolls = 5
+	for n := 0; n < maxPolls; n++ {
+		timeout := time.Duration((5 + rand.Intn(10))) * time.Millisecond
+		event, ok := kq.Poll(timeout)
+		if !ok {
+			continue
+		}
+		log.Println("received event", event)
+		break
+	}
+}
+
+func MustGetRandomItem[T any](items []T) T {
+	if len(items) == 0 {
+		panic(errors.New("empty slice"))
+	}
+	return items[rand.Intn(len(items))]
+}
