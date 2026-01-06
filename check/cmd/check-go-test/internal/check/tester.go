fe6536f9bdcc8fb10a6a5626107af8d1b3dbd31d
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-01-06 16:42:06 -0600

    check-go-test: decode build events
    The output of `go test -json` include both build and test events. This
    change brings support for build events. It makes check-go-test fail if
    the build fails.
    
    Issue #15

--- check/cmd/check-go-test/internal/check/tester.go
+++ check/cmd/check-go-test/internal/check/tester.go
@@ -6,9 +6,12 @@
 package check
 
 import (
+	"bufio"
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io"
 	"iter"
 	"os/exec"
@@ -24,6 +27,16 @@ type EventID string
 type Event struct {
 	BuildEvent *build.Event
 	TestEvent  *test.TestEvent
+}
+
+func (e *Event) Fail() bool {
+	if e.BuildEvent != nil && e.BuildEvent.Action == build.ActionFail {
+		return true
+	}
+	if e.TestEvent != nil && e.TestEvent.Action == test.ActionFail {
+		return true
+	}
+	return false
 }
 
 // Tester runs `go test` on packages and groups events by event ids. It also
@@ -55,7 +68,7 @@ func (t *Tester) Test(pkg string) error {
 	}
 	defer cmd.Wait()
 	for id, e := range decodeEvents(stdout) {
-		if e.TestEvent.Action == test.ActionFail {
+		if e.Fail() {
 			t.fails = append(t.fails, id)
 		}
 		ee := t.events[id]
@@ -66,19 +79,57 @@ func (t *Tester) Test(pkg string) error {
 
 func decodeEvents(r io.Reader) iter.Seq2[EventID, *Event] {
 	return func(yield func(EventID, *Event) bool) {
-		dec := json.NewDecoder(r)
-		for {
-			e := new(test.TestEvent)
-			if err := dec.Decode(e); err == io.EOF {
-				break
-			} else if err != nil {
+		scanner := bufio.NewScanner(r)
+		for scanner.Scan() {
+			b := scanner.Bytes()
+			e, err := JSONUnmarshal(b)
+			if err != nil {
 				break
 			}
-			if !yield(NewEventID(e), &Event{TestEvent: e}) {
+			if !yield(NewEventID(e), e) {
 				break
 			}
 		}
 	}
+}
+
+var (
+	jsonPrefix       = []byte(`{`)
+	buildEventMarker = []byte(`"Action":"build-`)
+)
+
+var ErrNotJSON = errors.New("not JSON")
+
+func JSONUnmarshal(b []byte) (*Event, error) {
+	// Build output may include non-JSON lines `go help buildjson`
+	if !bytes.HasPrefix(b, jsonPrefix) {
+		return nil, fmt.Errorf("parse %q: %w", b, ErrNotJSON)
+	}
+	if bytes.Contains(b, buildEventMarker) {
+		return jsonUnmarshalBuildEvent(b)
+	}
+	return jsonUnmarshalTestEvent(b)
+}
+
+func jsonUnmarshalBuildEvent(b []byte) (*Event, error) {
+	e := new(build.Event)
+	if err := json.Unmarshal(b, &e); err != nil {
+		return nil, err
+	}
+	return &Event{
+		BuildEvent: e,
+	}, nil
+
+}
+
+func jsonUnmarshalTestEvent(b []byte) (*Event, error) {
+	e := new(test.TestEvent)
+	if err := json.Unmarshal(b, &e); err != nil {
+		return nil, err
+	}
+	return &Event{
+		TestEvent: e,
+	}, nil
 }
 
 // VisitFails calls f on failed tests.
@@ -90,7 +141,25 @@ func (t *Tester) VisitFails(f func(*FailedTest)) {
 }
 
 // NewEventID constructs an EventID for a given event.
-func NewEventID(e *test.TestEvent) EventID {
+func NewEventID(e *Event) EventID {
+	var eid EventID
+	switch {
+	default:
+		// invalid event
+	case e.BuildEvent != nil:
+		eid = newEventIDFromBuildEvent(e.BuildEvent)
+	case e.TestEvent != nil:
+		eid = newEventIDFromTestEvent(e.TestEvent)
+	}
+	return eid
+}
+
+func newEventIDFromBuildEvent(e *build.Event) EventID {
+	id := e.ImportPath
+	return EventID(id)
+}
+
+func newEventIDFromTestEvent(e *test.TestEvent) EventID {
 	id := e.Test
 	if e.Package != "" {
 		id = filepath.Join(e.Package, e.Test)
@@ -108,15 +177,33 @@ type FailedTest struct {
 }
 
 func newFailedTest(ee []*Event) *FailedTest {
-	buf := new(bytes.Buffer)
-	var pkg, t string
-	for _, e := range ee {
-		switch e.TestEvent.Action {
+	var (
+		pkg, t string
+		buf    = new(bytes.Buffer)
+	)
+	collectBuildEvent := func(e *build.Event) {
+		switch e.Action {
+		case build.ActionFail:
+			pkg = e.ImportPath
+		case build.ActionOutput:
+			buf.WriteString(e.Output)
+		}
+	}
+	collectTestEvent := func(e *test.TestEvent) {
+		switch e.Action {
 		case test.ActionFail:
-			pkg = e.TestEvent.Package
-			t = e.TestEvent.Test
+			pkg = e.Package
+			t = e.Test
 		case test.ActionOutput:
-			buf.WriteString(e.TestEvent.Output)
+			buf.WriteString(e.Output)
+		}
+	}
+	for _, e := range ee {
+		switch {
+		case e.BuildEvent != nil:
+			collectBuildEvent(e.BuildEvent)
+		case e.TestEvent != nil:
+			collectTestEvent(e.TestEvent)
 		}
 	}
 	return &FailedTest{
--- check/cmd/check-go-test/internal/check/tester_test.go
+++ check/cmd/check-go-test/internal/check/tester_test.go
@@ -6,27 +6,135 @@
 package check_test
 
 import (
+	"errors"
 	"fmt"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
+	"github.com/skhal/lab/check/cmd/check-go-test/internal/build"
 	"github.com/skhal/lab/check/cmd/check-go-test/internal/check"
 	"github.com/skhal/lab/check/cmd/check-go-test/internal/test"
 )
 
+func TestEvent_Fail(t *testing.T) {
+	tests := []struct {
+		name  string
+		event *check.Event
+		want  bool
+	}{
+		{
+			name:  "build event action output",
+			event: withBuildEventAction(t, build.ActionOutput),
+		},
+		{
+			name:  "build event action fail",
+			event: withBuildEventAction(t, build.ActionFail),
+			want:  true,
+		},
+		{
+			name:  "test event action start",
+			event: withTestEventAction(t, test.ActionStart),
+		},
+		{
+			name:  "test event action run",
+			event: withTestEventAction(t, test.ActionRun),
+		},
+		{
+			name:  "test event action pause",
+			event: withTestEventAction(t, test.ActionPause),
+		},
+		{
+			name:  "test event action continue",
+			event: withTestEventAction(t, test.ActionContinue),
+		},
+		{
+			name:  "test event action pass",
+			event: withTestEventAction(t, test.ActionPass),
+		},
+		{
+			name:  "test event action benchmark",
+			event: withTestEventAction(t, test.ActionBenchmark),
+		},
+		{
+			name:  "test event action fail",
+			event: withTestEventAction(t, test.ActionFail),
+			want:  true,
+		},
+		{
+			name:  "test event action output",
+			event: withTestEventAction(t, test.ActionOutput),
+		},
+		{
+			name:  "test event action skip",
+			event: withTestEventAction(t, test.ActionSkip),
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got := tc.event.Fail()
+			if got != tc.want {
+				t.Errorf("check.Event.Fail() got %v; want %v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestJSONUnmarshal(t *testing.T) {
+	tests := []struct {
+		name    string
+		b       string
+		want    *check.Event
+		wantErr error
+	}{
+		{
+			name:    "not json",
+			b:       "test",
+			wantErr: check.ErrNotJSON,
+		},
+		{
+			name: "build event",
+			b:    `{"ImportPath":"test","Action":"build-output"}`,
+			want: withBuildEvent(t, &build.Event{ImportPath: "test", Action: build.ActionOutput}),
+		},
+		{
+			name: "test event",
+			b:    `{"Package":"test","Action":"output"}`,
+			want: withTestEvent(t, &test.TestEvent{Package: "test", Action: test.ActionOutput}),
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := check.JSONUnmarshal([]byte(tc.b))
+			if !errors.Is(err, tc.wantErr) {
+				t.Errorf("check.JSONUnmarshal() unexpected error %q; want %q", err, tc.wantErr)
+				t.Log(tc.b)
+			}
+			if diff := cmp.Diff(tc.want, got); diff != "" {
+				t.Errorf("check.JSONUnmarshal() mismatch (-want, +got):\n%s", diff)
+			}
+		})
+	}
+}
+
 func TestNewEventID(t *testing.T) {
 	tests := []struct {
 		name  string
-		event *test.TestEvent
+		event *check.Event
 		want  check.EventID
 	}{
 		{
-			name:  "no package",
-			event: &test.TestEvent{Test: "test"},
+			name:  "build event",
+			event: withBuildEvent(t, &build.Event{ImportPath: "test"}),
 			want:  check.EventID("test"),
 		},
 		{
-			name:  "with package",
-			event: &test.TestEvent{Package: "package", Test: "test"},
+			name:  "test event no package",
+			event: withTestEvent(t, &test.TestEvent{Test: "test"}),
+			want:  check.EventID("test"),
+		},
+		{
+			name:  "test event with package",
+			event: withTestEvent(t, &test.TestEvent{Package: "package", Test: "test"}),
 			want:  check.EventID("package/test"),
 		},
 	}
@@ -41,8 +149,42 @@ func TestNewEventID(t *testing.T) {
 	}
 }
 
-type printableEvent test.TestEvent
+func withBuildEventAction(t *testing.T, a build.Action) *check.Event {
+	return &check.Event{
+		BuildEvent: &build.Event{
+			Action: a,
+		},
+	}
+}
+
+func withTestEventAction(t *testing.T, a test.Action) *check.Event {
+	return &check.Event{
+		TestEvent: &test.TestEvent{
+			Action: a,
+		},
+	}
+}
+
+type printableEvent check.Event
 
 func (e *printableEvent) String() string {
-	return fmt.Sprintf("{package:%q test:%q}", e.Package, e.Test)
+	if e.BuildEvent != nil {
+		be := e.BuildEvent
+		return fmt.Sprintf("{import-path:%q}", be.ImportPath)
+	}
+	if e.TestEvent != nil {
+		te := e.TestEvent
+		return fmt.Sprintf("{package:%q test:%q}", te.Package, te.Test)
+	}
+	return "{}"
+}
+
+func withBuildEvent(t *testing.T, e *build.Event) *check.Event {
+	t.Helper()
+	return &check.Event{BuildEvent: e}
+}
+
+func withTestEvent(t *testing.T, e *test.TestEvent) *check.Event {
+	t.Helper()
+	return &check.Event{TestEvent: e}
 }
