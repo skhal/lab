b7c522d9666d9588aea237e09925451aacb90686
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-01-08 08:22:22 -0600

    mdview: separate file stats and rendered markdown
    The new data structure Preview includes file stats and rendered HTML.
    
    Issue #42

--- x/mdview/internal/mdview/serve.go
+++ x/mdview/internal/mdview/serve.go
@@ -28,37 +28,69 @@ func listenAndServe(addr string) error {
 }
 
 func serveFile(w http.ResponseWriter, req *http.Request) {
-	data, err := renderMarkdown(req.URL.Path)
+	preview, err := renderMarkdown(req.URL.Path)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		log.Println(err)
 		return
 	}
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, string(data))
+	fmt.Fprint(w, string(preview.HTML))
+}
+
+type Preview struct {
+	Info *FileInfo
+	HTML []byte
+}
+
+type FileInfo struct {
+	Path    string
+	AbsPath string
 }
 
 const extMarkdown = ".md"
 
-func renderMarkdown(path string) ([]byte, error) {
+func renderMarkdown(path string) (*Preview, error) {
+	info, err := stat(path)
+	if err != nil {
+		return nil, err
+	}
+	html, err := readAndRender(info)
+	if err != nil {
+		return nil, err
+	}
+	return &Preview{
+		Info: info,
+		HTML: html,
+	}, nil
+}
+
+func stat(path string) (*FileInfo, error) {
 	if extMarkdown != filepath.Ext(path) {
-		return nil, fmt.Errorf("%s does not have a markdown extension", path)
+		return nil, fmt.Errorf("%s: not markdown", path)
 	}
 	pwd, err := os.Getwd()
 	if err != nil {
 		return nil, err
 	}
-	fname := filepath.Join(pwd, path)
-	fi, err := os.Stat(fname)
+	abspath := filepath.Join(pwd, path)
+	fi, err := os.Stat(abspath)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("%s: fail to stat", path)
 	}
 	if !fi.Mode().IsRegular() {
-		return nil, fmt.Errorf("%s is not a regular file", path)
+		return nil, fmt.Errorf("%s: not a regular file", path)
 	}
-	data, err := os.ReadFile(fname)
+	return &FileInfo{
+		Path:    path,
+		AbsPath: abspath,
+	}, nil
+}
+
+func readAndRender(info *FileInfo) ([]byte, error) {
+	data, err := os.ReadFile(info.AbsPath)
 	if err != nil {
-		return nil, fmt.Errorf("Failed to open %s", path)
+		return nil, fmt.Errorf("%s: fail to read", info.Path)
 	}
 	return Render(data), nil
 }
