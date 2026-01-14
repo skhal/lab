79ff2ea92f42569d65a076e547c184f549b8c4d2
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-01-14 06:36:19 -0600

    check-license: split run and check
    Move check license code into a separate file.
    
    NO_ISSUE

--- check/cmd/check-license/internal/license/run.go
+++ check/cmd/check-license/internal/license/run.go
@@ -6,12 +6,8 @@
 package license
 
 import (
-	"bytes"
-	"embed"
 	"errors"
 	"fmt"
-	"regexp"
-	"text/template"
 )
 
 var (
@@ -42,14 +38,6 @@ func (e *InvalidError) Is(target error) bool {
 	return target == ErrInvalid
 }
 
-var lineRx = regexp.MustCompile(`^([\t ]*(/[/\*]|[#"])?) Copyright`)
-
-var (
-	//go:embed data
-	embedFS      embed.FS
-	licenseTmpls = template.Must(template.New("licenses").ParseFS(embedFS, "data/license_*.txt"))
-)
-
 // ReadFileFn reads file and returns its content or error.
 type ReadFileFn = func(string) ([]byte, error)
 
@@ -70,63 +58,4 @@ func Run(cfg *Config, files []string) error {
 		}
 	}
 	return nil
-}
-
-const eol = '\n'
-
-func check(buf []byte) (err error) {
-	ln := 1
-	defer func() {
-		if errors.Is(err, ErrInvalid) {
-			err = NewInvalidError(ln)
-		}
-	}()
-	for len(buf) > 0 {
-		if ok, err := match(buf); ok {
-			return nil
-		} else if err != nil {
-			return err
-		}
-		idx := bytes.IndexByte(buf, eol)
-		if idx == -1 {
-			break
-		}
-		idx += 1 // skip eol
-		buf = buf[idx:]
-		ln += 1
-	}
-	return ErrNotFound
-}
-
-func match(buf []byte) (ok bool, err error) {
-	matches := lineRx.FindSubmatch(buf)
-	if matches == nil {
-		return
-	}
-	blockRx, err := compileBlockRx(matches[1])
-	if err != nil {
-		return
-	}
-	if !blockRx.Match(buf) {
-		return false, ErrInvalid
-	}
-	return true, nil
-}
-
-func compileBlockRx(prefix []byte) (*regexp.Regexp, error) {
-	data := struct {
-		Prefix          string
-		EmptyLinePrefix string
-	}{
-		Prefix: string(prefix),
-	}
-	// The separator line is empty in comment blocks, e.g. HTML.
-	if len(bytes.TrimSpace(prefix)) != 0 {
-		data.EmptyLinePrefix = data.Prefix
-	}
-	var b bytes.Buffer
-	if err := licenseTmpls.ExecuteTemplate(&b, "license_bsd.txt", data); err != nil {
-		return nil, err
-	}
-	return regexp.Compile("^" + b.String()) // must match the beginning
 }
