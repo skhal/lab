f38645a15061b8f5d375cf239528b5d87d24c2a9
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-01-22 07:09:40 -0600

    rss: add a proto schema for a feed
    Issue #47

---
+++ x/rss/cmd/rssreader/internal/pb/gen.go
@@ -0,0 +1,9 @@
+// Copyright 2026 Samvel Khalatyan. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package pb
+
+//go:generate -command protoc_cmd protoc --proto_path=. -I=../../../../../../ --go_out=. --go_opt=paths=source_relative
+//go:generate protoc_cmd feed.proto
