e60dce178119d218152d56500662fe2a798ffab5
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-01-23 16:16:42 -0600

    x/ast: dump an ast of a Go file
    NO_ISSUE

---
+++ x/ast/main.go
@@ -0,0 +1,33 @@
+// Copyright 2026 Samvel Khalatyan. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Ast pases a Go file and prints Abstract Syntax Tree.
+//
+// Usage:
+//
+//	ast file
+package main
+
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "usage: %s file\n", filepath.Base(os.Args[0]))
+	}
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, os.Args[1], nil, 0)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	ast.Print(fset, f)
+}
