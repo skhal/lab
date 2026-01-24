d9b5fad25f8ddc8617237fb79cf473a78e27a958
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-01-24 15:19:36 -0600

    check-go-test: skip testdata
    NO_ISSUE

--- check/cmd/check-go-test/internal/check/run.go
+++ check/cmd/check-go-test/internal/check/run.go
@@ -10,6 +10,7 @@ import (
 	"fmt"
 	"path/filepath"
 	"slices"
+	"strings"
 )
 
 // ErrTest indicates an error in running go tests.
@@ -18,6 +19,9 @@ var ErrTest = errors.New("test error")
 // Run runs `go test` on packages for listed files.
 func Run(files ...string) (err error) {
 	packages := slices.Collect(Unique(Paths(FilterFunc(slices.Values(files), IsGoFile))))
+	packages = slices.DeleteFunc(packages, func(p string) bool {
+		return strings.Contains(p, "/testdata/")
+	})
 	if len(packages) == 0 {
 		return
 	}
