5c6acddaf5261a330cdbc0e6a7977ab50616b54f
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-01-27 05:13:01 -0600

    nvim: group settings by topic
    Issue #60

--- home/nvim/init.lua
+++ home/nvim/init.lua
@@ -6,17 +6,19 @@
 -- keep-sorted start
 vim.o.colorcolumn = "80"
 vim.o.cursorline = true
-vim.o.expandtab = true
-vim.o.listchars = "eol:¬,extends:›,precedes:‹,space:░,tab:«–»,trail:•"
 vim.o.number = true
+-- keep-sorted end
+
+vim.o.listchars = "eol:¬,extends:›,precedes:‹,space:░,tab:«–»,trail:•"
+vim.keymap.set({ "n" }, "<leader>l", "<esc>:set list!<cr>")
+
+-- keep-sorted start
+vim.o.expandtab = true
 vim.o.shiftwidth = 2
 vim.o.smartindent = true
 vim.o.softtabstop = 2
 vim.o.tabstop = 2
 -- keep-sorted end
-
-vim.keymap.set({ "n" }, "<leader>l", "<esc>:set list!<cr>")
-
 vim.api.nvim_create_autocmd("Filetype", {
 	group = vim.api.nvim_create_augroup("Indents", { clear = true }),
 	pattern = { "go", "make" },
