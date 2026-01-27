e4d71b969a2a2f9530572939bb13cd2620fd6a8b
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-01-26 19:30:02 -0600

    nvim: transfer root vimrc to neovim init.lua
    Close #60

--- home/nvim/init.lua
+++ home/nvim/init.lua
@@ -2,3 +2,26 @@
 --
 -- Use of this source code is governed by a BSD-style
 -- license that can be found in the LICENSE file.
+
+-- keep-sorted start
+vim.o.autoindent = true
+vim.o.colorcolumn = "80"
+vim.o.cursorline = true
+vim.o.expandtab = true
+vim.o.hlsearch = true
+vim.o.incsearch = true
+vim.o.listchars = "eol:¬,extends:›,precedes:‹,space:░,tab:«–»,trail:•"
+vim.o.number = true
+vim.o.shiftwidth = 2
+vim.o.smartindent = true
+vim.o.softtabstop = 2
+vim.o.tabstop = 2
+-- keep-sorted end
+
+vim.keymap.set({ "n" }, "<leader>l", "<esc>:set list!<cr>")
+
+vim.api.nvim_create_autocmd("Filetype", {
+	group = vim.api.nvim_create_augroup("Indents", { clear = true }),
+	pattern = { "go", "make" },
+	command = "set noexpandtab",
+})
