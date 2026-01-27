3ed3e1b6163ae4069df9cf42a2cbecbb4e5c32bc
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-01-27 12:34:25 -0600

    nvim: replace year and hodler in templates
    Issue #63

--- nvim/plugin/lab-skeleton/lua/lab-skeleton.lua
+++ nvim/plugin/lab-skeleton/lua/lab-skeleton.lua
@@ -17,8 +17,43 @@ function M.setup(opts)
 		group = M.augroup,
 		desc = "Load template",
 		pattern = { "*.lua" },
-		command = "0r " .. M.skel_path .. "/new.lua",
+		callback = function(ev)
+			M.load(ev)
+		end,
 	})
+end
+
+function M.load(ev)
+	local ext = vim.fn.fnamemodify(ev.file, ":e")
+	local skel_path = M.skel_path .. "/new." .. ext
+	if not (vim.uv or vim.loop).fs_stat(skel_path) then
+		vim.api.nvim_echo({
+			{ ("skeleton %s: does not exist.\n"):format(skel_path), "ErrorMsg" },
+		}, true, {})
+		return
+	end
+	local obj = vim.system({ "git", "config", "--get", "user.name" }, { text = true }):wait()
+	if obj.code ~= 0 then
+		vim.api.nvim_echo({
+			{ "Can't get user name", "ErrorMsg" },
+		}, true, {})
+		return
+	end
+	local holder = (obj.stdout):gsub("+%s+", "")
+	vim.api.nvim_echo({
+		{ ("Holder: %s"):format(holder), "Normal" },
+	}, true, {})
+	local substitutes = {
+		["{{ *\\.Year *}}"] = os.date("%Y"),
+		["{{ *\\.Holder *}}"] = holder,
+	}
+	vim.cmd("0r " .. skel_path)
+	for key, val in pairs(substitutes) do
+		vim.cmd("silent! %s/" .. key .. "/" .. val)
+	end
+	vim.api.nvim_echo({
+		{ ("skel: %s\n"):format(skel_path), "WarningMsg" },
+	}, true, {})
 end
 
 return M
