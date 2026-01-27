f0543fb76602d7b8bbec67385f18049e7fc659d2
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-01-27 14:40:09 -0600

    lab-skeleton: add C and C++ templates
    Issue #63

---
+++ nvim/plugin/lab-skeleton/share/new.h
@@ -0,0 +1,15 @@
+// Copyright {{year}} {{holder}}. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#ifndef{{guard } }
+#define{{guard } }
+
+namespace {
+{namespace}
+}  // namespace
+{
+}  // namespace {{namespace}}
+
+#endif  // {{guard}}
