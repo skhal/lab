f6dec1d41aee58ca0209fb7de5b8fa81edb85530
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-01-27 14:21:43 -0600

    nvim: lab-skeleton: break load into multiple functions
    Issue #63

--- nvim/plugin/lab-skeleton/lua/lab-skeleton.lua
+++ nvim/plugin/lab-skeleton/lua/lab-skeleton.lua
@@ -23,32 +23,14 @@ function M.setup(opts)
 	})
 end
 
-function M.load(ev)
-	local ext = vim.fn.fnamemodify(ev.file, ":e")
-	local skel_path = M.skel_path .. "/new." .. ext
-	if not (vim.uv or vim.loop).fs_stat(skel_path) then
-		vim.api.nvim_echo({
-			{ ("skeleton %s: does not exist.\n"):format(skel_path), "ErrorMsg" },
-		}, true, {})
-		return
-	end
-	local obj = vim.system({ "git", "config", "--get", "user.name" }, { text = true }):wait()
-	if obj.code ~= 0 then
-		vim.api.nvim_echo({
-			{ "Can't get user name", "ErrorMsg" },
-		}, true, {})
-		return
-	end
-	local year = os.date("%Y")
-	local holder = (obj.stdout):gsub("+%s+", "")
-	local substitutes = {
-		["{{year}}"] = year,
-		["{{holder}}"] = holder,
-	}
-	vim.cmd("0r " .. skel_path)
-	for key, val in pairs(substitutes) do
+function load_skeleton(file, subs)
+	vim.cmd("0r " .. file)
+	for key, val in pairs(subs) do
 		vim.cmd("silent! %s/" .. key .. "/" .. val)
 	end
+end
+
+function position_cursor()
 	for line_num, line in ipairs(vim.api.nvim_buf_get_lines(0, 0, -1, false)) do
 		local from, to = line:find("{{cursor}}")
 		if from ~= nil then
@@ -57,9 +39,45 @@ function M.load(ev)
 			break
 		end
 	end
-	vim.api.nvim_echo({
-		{ ("skel: %s\nyear: %d\nholder: %s"):format(skel_path, year, holder), "Normal" },
-	}, true, {})
+end
+
+function M.load(ev)
+	local ok, skel_path = pcall(M.pick_skeleton, ev.file)
+	if not ok then
+		vim.api.nvim_echo({ { skel_path, "ErrorMsg" } }, true, {})
+		return
+	end
+	local ok, subs = pcall(M.gen_substitutes)
+	if not ok then
+		vim.api.nvim_echo({ { subs, "ErrorMsg" } }, true, {})
+		return
+	end
+	local ok, err = pcall(load_skeleton, skel_path, subs)
+	if not ok then
+		vim.api.nvim_echo({ { err, "ErrorMsg" } }, true, {})
+		return
+	end
+	local ok, err = pcall(position_cursor)
+end
+
+function M.pick_skeleton(file)
+	local ext = vim.fn.fnamemodify(file, ":e")
+	local skel_path = M.skel_path .. "/new." .. ext
+	if not (vim.uv or vim.loop).fs_stat(skel_path) then
+		error(("skeleton %s: does not exist."):format(skel_path))
+	end
+	return skel_path
+end
+
+function M.gen_substitutes()
+	local cmd = vim.system({ "git", "config", "--get", "user.name" }, { text = true }):wait()
+	if cmd.code ~= 0 then
+		error("git-config: can't get user.name")
+	end
+	return {
+		["{{year}}"] = os.date("%Y"),
+		["{{holder}}"] = (cmd.stdout):gsub("+%s+", ""),
+	}
 end
 
 return M
