9436874ae6a9fb68760ca419920de16b356219e9
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-01-28 13:32:06 -0600

    lab-skeleton: clean up config script
    Merge echo messages and use `vim.fs` to join path.
    
    Issue #63

--- home/nvim/lua/config/lab-skeleton.lua
+++ home/nvim/lua/config/lab-skeleton.lua
@@ -3,13 +3,11 @@
 -- Use of this source code is governed by a BSD-style
 -- license that can be found in the LICENSE file.
 
-local lab_skeleton_path = vim.fn.stdpath("data") .. "/lab-skeleton"
-if not (vim.uv or vim.loop).fs_stat(lab_skeleton_path) then
+local lab_skeleton_path = vim.fs.joinpath(vim.fn.stdpath("data"), "lab-skeleton")
+if not vim.uv.fs_stat(lab_skeleton_path) then
 	vim.api.nvim_echo({
-		{ "Plugin lab-skeleton is not installed.\n", "ErrorMsg" },
-		{ lab_skeleton_path, "ErrorMsg" },
+		{ ("Plugin lab-skeleton is not installed.\n%s"):format(lab_skeleton_path), "ErrorMsg" },
 	}, true, {})
-	vim.api.finish()
 	return {}
 end
 
