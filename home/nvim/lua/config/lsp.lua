8e135377b23d1aa75b987ca9601a26205539357c
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-01-29 10:57:46 -0600

    nvim: enable LSP for Go
    Close #67

--- home/nvim/lua/config/lsp.lua
+++ home/nvim/lua/config/lsp.lua
@@ -3,7 +3,10 @@
 -- Use of this source code is governed by a BSD-style
 -- license that can be found in the LICENSE file.
 
+-- keep-sorted start
+vim.lsp.enable("gopls")
 vim.lsp.enable("lua_ls")
+-- keep-sorted end
 
 vim.keymap.set("n", "gK", function()
 	local new_config = not vim.diagnostic.config().virtual_lines
