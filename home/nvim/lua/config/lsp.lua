8d5cba95577598f2e9445cd2fc890795d64f273f
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-01-29 10:52:52 -0600

    nvim: use nvim-lspconfig for lua
    Issue #75

--- home/nvim/lua/config/lsp.lua
+++ home/nvim/lua/config/lsp.lua
@@ -3,25 +3,4 @@
 -- Use of this source code is governed by a BSD-style
 -- license that can be found in the LICENSE file.
 
--- ref: https://neovim.io/doc/user/lsp.html
-vim.lsp.config["lua_ls"] = {
-	-- Command and arguments to start the server.
-	cmd = { "lua-language-server" },
-	-- Filetypes to automatically attach to.
-	filetypes = { "lua" },
-	-- Sets the "workspace" to the directory where any of these files is found.
-	-- Files that share a root directory will reuse the LSP server connection.
-	-- Nested lists indicate equal priority, see |vim.lsp.Config|.
-	root_markers = { { ".luarc.json", ".luarc.jsonc" }, ".git" },
-	-- Specific settings to send to the server. The schema is server-defined.
-	-- Example: https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
-	settings = {
-		Lua = {
-			runtime = {
-				version = "LuaJIT",
-			},
-		},
-	},
-}
-
 vim.lsp.enable("lua_ls")
