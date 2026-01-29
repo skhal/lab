f84e5639db68a89c31ef28e33cf3da79237507ab
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-01-29 10:54:01 -0600

    nvim: toggle LSP diagnostics on gK
    Issue #75

--- home/nvim/lua/config/lsp.lua
+++ home/nvim/lua/config/lsp.lua
@@ -4,3 +4,8 @@
 -- license that can be found in the LICENSE file.
 
 vim.lsp.enable("lua_ls")
+
+vim.keymap.set("n", "gK", function()
+	local new_config = not vim.diagnostic.config().virtual_lines
+	vim.diagnostic.config({ virtual_lines = new_config })
+end, { desc = "Toggle diagnostic virtual_lines" })
