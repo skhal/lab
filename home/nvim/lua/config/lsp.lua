fc7f223eaadff5373528f4eb39fe0dd7d0fc40c0
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-01-30 12:16:19 -0600

    nvim: disable lsp completion auto-insert
    Do not insert the first suggested completion result. Instead, let
    drop-down menu list options but continue typing. Use C-n or C-p to
    select desired item if necessary.
    
    Issue #70

--- home/nvim/lua/config/lsp.lua
+++ home/nvim/lua/config/lsp.lua
@@ -11,6 +11,8 @@ vim.lsp.enable("protols") -- NOLINT
 vim.lsp.enable("typos_lsp")
 -- keep-sorted end
 
+vim.cmd([[set completeopt+=menuone,noselect,popup]])
+
 vim.keymap.set("n", "gK", function()
 	local new_config = not vim.diagnostic.config().virtual_lines
 	vim.diagnostic.config({ virtual_lines = new_config })
