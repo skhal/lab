312c752ec3d517542caf79d836657f2691a75e87
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-01-29 19:19:31 -0600

    nvim: add related files for C++
    Issue #71

--- home/nvim/after/ftplugin/cpp.lua
+++ home/nvim/after/ftplugin/cpp.lua
@@ -5,4 +5,43 @@
 
 vim.bo.equalprg = "clang-format21 -assume-filename=%"
 
-vim.api.nvim_buf_set_keymap(0, "n", "<localleader>rs", "<cmd>LspClangdSwitchSourceHeader<cr>", {})
+local M = {
+	header = function()
+		local f = vim.fn.expand("%")
+		if not f:find("%.cc$") then
+			return
+		end
+		f = f:gsub("_test%.cc$", ".h")
+		f = f:gsub("%.cc$", ".h")
+		vim.cmd.edit(f)
+	end,
+	source = function()
+		local f = vim.fn.expand("%")
+		if f:find("%.h$") then
+			f = f:gsub("%.h$", ".cc")
+		elseif f:find("_test%.cc$") then
+			f = f:gsub("_test%.cc$", ".cc")
+		else
+			return
+		end
+		vim.cmd.edit(f)
+	end,
+	test = function()
+		local f = vim.fn.expand("%")
+		if f:find("_test%.cc$") then
+			return
+		end
+		if f:find("%.h$") then
+			f = f:gsub("%.h$", "_test.cc")
+		elseif f:find("%.cc") then
+			f = f:gsub("%.cc$", "_test.cc")
+		else
+			return
+		end
+		vim.cmd.edit(f)
+	end,
+}
+
+vim.keymap.set("n", "<localleader>rh", M.header, { buffer = true })
+vim.keymap.set("n", "<localleader>rs", M.source, { buffer = true })
+vim.keymap.set("n", "<localleader>rt", M.test, { buffer = true })
