793590958650bd62b03780e31978f9186f2d4b62
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-01-30 20:46:23 -0600

    nvim: format buffers with <localleader>fc
    The change uses https://github.com/stevearc/conform.nvim to format
    files.
    
    In a nutshell, it runs formatter on the buffer and uses vim.diff() to
    generate edit steps, passed through LSP to edit the buffer. See
    lua/conform/runner.lua.
    
    Issue #61

---
+++ home/nvim/lua/config/format.lua
@@ -0,0 +1,37 @@
+-- Copyright 2026 Samvel Khalatyan. All rights reserved.
+--
+-- Use of this source code is governed by a BSD-style
+-- license that can be found in the LICENSE file.
+
+local conform = require("conform")
+
+conform.setup({
+	formatters = {
+		["clang-format"] = {
+			command = "clang-format21",
+		},
+	},
+	formatters_by_ft = {
+		-- keep-sorted start
+		bzl = { "buildifier" },
+		c = { "clang-format" },
+		cpp = { "clang-format" },
+		go = { "goimports" },
+		html = { "prettier" },
+		json = { "prettier" },
+		lua = { "stylua" },
+		markdown = { "markdownfmt" },
+		proto = { "clang-format" },
+		sh = { "shfmt" },
+		yaml = { "yamlfmt" },
+		-- keep-sorted end
+	},
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(_)
+		vim.keymap.set("n", "<localleader>fc", conform.format, { buffer = true })
+	end,
+})
+
+vim.bo.formatexpr = "v:lua.require'conform'.formatexpr()"
