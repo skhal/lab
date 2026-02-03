25b928a24afb0a10d5bb38edd4569354061ea4df
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-02-03 10:03:11 -0600

    nvim : assert when getting lsp client
    NO_ISSUE

--- home/nvim/lua/config/complete.lua
+++ home/nvim/lua/config/complete.lua
@@ -64,7 +64,7 @@ vim.lsp.config("*", {
 
 vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(args)
-		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
 		vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
 	end,
 })
