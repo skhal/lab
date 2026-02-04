6e5e4c1cd5a977c37d3baa9e4525a7571fd00ad3
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-02-04 11:06:02 -0600

    lab-skeleton: add a template for C++ main apps
    The files have `main.cc` suffix.
    
    NO_ISSUE

---
+++ nvim/plugin/lab-skeleton/share/new_main.cc
@@ -0,0 +1,8 @@
+// Copyright {{year}} {{holder}}. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+int main() {
+  {{cursor}}
+}
