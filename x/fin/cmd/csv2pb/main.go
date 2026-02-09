5cd4d236bf3f321040c1e44c36f6a50a9653ac4d
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-02-09 11:42:30 -0600

    x/fin/csv2pb: add output file flag
    The new flag `-o file` sets the output file to save serialized market
    data.
    
    Issue #87

--- x/fin/cmd/csv2pb/main.go
+++ x/fin/cmd/csv2pb/main.go
@@ -15,48 +15,77 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/skhal/lab/x/fin/internal/csv"
+	"github.com/skhal/lab/x/fin/internal/pb"
 	"google.golang.org/protobuf/proto"
 )
 
+const permWrite = 0664
+
 func main() {
-	if err := run(os.Args[1:]); err != nil {
+	if err := run(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
 
-func run(files []string) error {
-	if len(files) < 1 {
-		return fmt.Errorf("missing csv input file")
-	}
-	if len(files) != 1 {
-		return fmt.Errorf("need only one file")
-	}
-	return runFile(files[0])
-}
-
-func runFile(file string) error {
-	f, err := os.Open(file)
+func run() error {
+	ifile, ofile, err := parseFlags()
 	if err != nil {
 		return err
+	}
+	m, err := parseFile(ifile)
+	if err != nil {
+		return err
+	}
+	return writeFile(m, ofile)
+}
+
+func parseFlags() (ifile, ofile string, err error) {
+	flag.Usage = func() {
+		out := flag.CommandLine.Output()
+		fmt.Fprintf(out, "usage: %s [flags] file\n", filepath.Base(os.Args[0]))
+		fmt.Fprintln(out)
+		fmt.Fprintf(out, "flags:\n")
+		flag.PrintDefaults()
+	}
+	flag.StringVar(&ofile, "o", "", "output file")
+	flag.Parse()
+	if ofile == "" {
+		err = errors.New("missing output file")
+		return
+	}
+	if flag.NArg() != 1 {
+		err = errors.New("missing input file")
+		return
+	}
+	ifile = flag.Arg(0)
+	return
+}
+
+func parseFile(file string) (*pb.Market, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
 	}
 	defer func() {
 		if err := f.Close(); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
 	}()
-	m, err := csv.Read(f)
-	if err != nil {
-		return err
-	}
+	return csv.Read(f)
+}
+
+func writeFile(m *pb.Market, name string) error {
 	b, err := proto.Marshal(m)
 	if err != nil {
 		return err
 	}
-	_, err = os.Stdout.Write(b)
-	return err
+	return os.WriteFile(name, b, permWrite)
 }
