be342a2b4360e575bc7c8b51065b0afe9b8ab218
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-02-26 08:34:45 -0600

    nvim: offer select menu to choose source or test from doc.go
    When switching to a source or test file from doc.go, offer a menu of
    options, because doc.go is for package documentation and does not have a
    related source or test.
    
    Close #154

--- home/nvim/after/ftplugin/go.lua
+++ home/nvim/after/ftplugin/go.lua
@@ -8,6 +8,49 @@ vim.bo.equalprg = "goimports"
 vim.bo.expandtab = false
 -- keep-sorted end
 
+local function select_source(file)
+	local opt_nosuf = false
+	local opt_list = true
+	local files = vim.fn.globpath(vim.fs.dirname(file), "*.go", opt_nosuf, opt_list)
+	files = vim.tbl_filter(function(item)
+		if item:find("doc%.go$") then
+			return false
+		end
+		if item:find("_test%.go$") then
+			return false
+		end
+		return true
+	end, files)
+	if not next(files) then
+		vim.api.nvim_echo({
+			{ "no source files found" },
+		}, true, {})
+		return
+	end
+	vim.ui.select(files, {
+		prompt = "Select source:",
+	}, function(choice)
+		vim.cmd.edit(choice)
+	end)
+end
+
+local function select_test(file)
+	local opt_nosuf = false
+	local opt_list = true
+	local files = vim.fn.globpath(vim.fs.dirname(file), "*_test.go", opt_nosuf, opt_list)
+	if not next(files) then
+		vim.api.nvim_echo({
+			{ "no test files found" },
+		}, true, {})
+		return
+	end
+	vim.ui.select(files, {
+		prompt = "Select test:",
+	}, function(choice)
+		vim.cmd.edit(choice)
+	end)
+end
+
 local M = {
 	doc = function()
 		local f = vim.fn.expand("%")
@@ -19,6 +62,10 @@ local M = {
 	end,
 	source = function()
 		local f = vim.fn.expand("%")
+		if f:find("doc%.go$") then
+			select_source(f)
+			return
+		end
 		if not f:find("_test%.go$") then
 			return
 		end
@@ -27,6 +74,10 @@ local M = {
 	end,
 	test = function()
 		local f = vim.fn.expand("%")
+		if f:find("doc%.go$") then
+			select_test(f)
+			return
+		end
 		if f:find("_test%.go$") then
 			return
 		end
