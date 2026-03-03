ce8ed9e06567dcd2e30f253c68b41ad6900da471
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-03-03 09:28:05 -0600

    ostep: review fork example
    NO_ISSUE

--- book/ostep/process/fork/main.c
+++ book/ostep/process/fork/main.c
@@ -3,26 +3,35 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Fork shows how to fork a child process.
+// Fork shows how to start a new process.
+//
+// Output (non-deterministic order after the first line):
+// 10292 prepare for fork
+// 10292 forked a child 10432
+// 10292 done
+// 10432 in child
+// 10432 done
 
 #include <err.h>
 #include <stdio.h>
 #include <sys/types.h>
+#include <sysexits.h>
 #include <unistd.h>
 
 int main() {
   pid_t pid;
-  printf("[%d]: prepare for fork\n", getpid());
+  printf("%d prepare for fork\n", getpid());
   switch (pid = fork()) {
     case -1:  // error
-      err(1, "parent pid %d", getpid());
+      err(EX_OSERR, "fork %d", getpid());
       break;
     case 0:  // child
-      printf("[%d]: in child\n", getpid());
+      printf("%d in child\n", getpid());
       break;
     default:  // parent
-      printf("[%d]: forked a child [%d]\n", getpid(), pid);
+      printf("%d forked a child %d\n", getpid(), pid);
       break;
   }
+  printf("%d done\n", getpid());
   return 0;
 }
