58276a426a6b02d562621d2470d00cc2bf91ec02
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-03-03 10:14:35 -0600

    ostep: review exec(2) example
    NO_ISSUE

--- book/ostep/process/exec/main.c
+++ book/ostep/process/exec/main.c
@@ -3,36 +3,52 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Exec shows how to run a program from a process.
+//
+// Output:
+// 81980 start
+// 81980 forked a child 82167
+// 82167 child
+// Tue Mar  3 10:10:31 CST 2026
+// 81980 child done 82167 [wait() = 82167]
+// 81980 done
+
 #include <err.h>
+#include <stddef.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sysexits.h>
 #include <unistd.h>
 
+char COMMAND[] = "/bin/date";
+
 int main() {
-  pid_t pid;
-  printf("[%d] start\n", getpid());
+  pid_t pid, wpid;
+  printf("%d start\n", getpid());
   switch (pid = fork()) {
     case -1:  // fail
-      err(1, "parent %d", getpid());
+      err(EX_OSERR, "fork %d", getpid());
       break;
     case 0: {  // child
+      printf("%d child\n", getpid());
       char* argv[2];
-      argv[0] = strdup("/bin/date");
+      argv[0] = strdup(COMMAND);
       argv[1] = NULL;
       if (execv(argv[0], argv) == -1) {
-        err(1, "[%d]: child: exec %s", getpid(), argv[0]);
-        exit(1);
+        err(EX_OSERR, "%d exec %s", getpid(), argv[0]);
       }
       break;
     }
     default:  // parent
-      printf("[%d] forked child [%d]\n", getpid(), pid);
-      pid_t wpid = wait(NULL);
-      printf("[%d] wait: rc %d\n", getpid(), wpid);
+      printf("%d forked a child %d\n", getpid(), pid);
+      if ((wpid = wait(NULL)) == -1) {
+        err(EX_OSERR, "wait %d\n", getpid());
+      }
+      printf("%d child done %d [wait() = %d]\n", getpid(), pid, wpid);
       break;
   }
+  printf("%d done\n", getpid());
   return 0;
 }
