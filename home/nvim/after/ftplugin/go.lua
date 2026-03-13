bacf98b101c0d4d43a45bf47cce469d5df28718c
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-03-13 15:37:01 -0500

    nvim: group methods by receiver type in the location list
    This change groups methods by the receiver type, similar to ho Google
    code search does it in th Outline block:
    
        https://cs.opensource.google/go/go/+/master:src/bufio/bufio.go
    
    The order of structure entiries is: fields, methods.
    
    Go code allows to declare methods before the receiver type. This change
    accounts for this by adding such methods to the receiver type.
    
    The entities other than methods and fields, remain in the declaration
    order.
    
    Issue #91

--- home/nvim/after/ftplugin/go.lua
+++ home/nvim/after/ftplugin/go.lua
@@ -97,6 +97,124 @@ vim.keymap.set("n", "<localleader>rd", RelatedFile.doc, { buffer = true })
 vim.keymap.set("n", "<localleader>rs", RelatedFile.source, { buffer = true })
 vim.keymap.set("n", "<localleader>rt", RelatedFile.test, { buffer = true })
 
+-- receiverType extracts structure name from the identifier. It is the
+-- receiver type in case of a method identifier, or the identifier.
+local function receiverType(ident)
+	local name = ident:match("^%(%*?(%w+)%)")
+	if name ~= nil then
+		return name
+	end
+	error(("failed to get the receiver type: %s"):format(ident))
+end
+
+-- LocationTree is a hierarchy of location items with structure fields and
+-- methods stored in a structure block. It helps group methods by structure
+-- type in method declaration order, see [LocationTree.Add].
+local LocationTree = {}
+
+function LocationTree:new()
+	local o = {
+		items = {}, -- top-level location items except fields and methods
+		structs = {}, -- a map of struct type to a table with fields and methods
+		lastStructName = nil, -- last structure type for fields
+		-- pending is a list of structure types that had a method declaration but
+		-- no structure definition yet. The following code is a valid Go code
+		-- with method declared before the receiver type:
+		--
+		--	func (Foo) MethodA() {} // Foo was not declared yet
+		--											    // pending={Foo={methods={{text=MethodA}}}}
+		--	...
+		--	type Foo struct {}
+		pending = {},
+	}
+	setmetatable(o, self)
+	self.__index = self
+	return o
+end
+
+-- Add introduces an item into the LocationTree. It stores field and method
+-- items under structures, in the order of declaration. Everything else goes
+-- into the items list.
+function LocationTree:Add(item)
+	item.ident = item.text:gsub("^%[%w+%] ", "")
+	if item.kind == "Struct" then
+		self:addStruct(item)
+	elseif item.kind == "Field" then
+		self:addField(item)
+		return
+	elseif item.kind == "Method" then
+		self:addMethod(item)
+		return
+	end
+	table.insert(self.items, item)
+end
+
+-- addStruct registers a structure to receive fields and methods. Keep in mind
+-- that the structure may already exist if method declarations precede type
+-- declaration.
+function LocationTree:addStruct(item)
+	if self.structs[item.ident] then
+		self.pending[item.ident] = nil
+	else
+		self:registerStruct(item.ident)
+	end
+	self.lastStructName = item.ident
+end
+
+function LocationTree:registerStruct(name)
+	local struct = {
+		fields = {},
+		methods = {},
+	}
+	self.structs[name] = struct
+	return struct
+end
+
+-- addField attaches a field to the structure.
+function LocationTree:addField(item)
+	table.insert(self.structs[self.lastStructName].fields, item)
+end
+
+-- addMethod attaches a method to the receiver type. Keep in mind that a method
+-- can be declared before the receiver. Is so, addMethod marks the structure
+-- "pending".
+function LocationTree:addMethod(item)
+	local name = receiverType(item.ident)
+	local struct = self.structs[name]
+	if not struct then
+		struct = self:registerStruct(name)
+		self.pending[name] = true
+	end
+	table.insert(struct.methods, item)
+end
+
+-- Items flattens the items list with every structure expanded with field and
+-- then methods. Any pending structures, go to the end of the location list.
+function LocationTree:Items()
+	local items = {}
+	for _, v in ipairs(self.items) do
+		if v.kind == "Struct" then
+			table.insert(items, v)
+			local struct = self.structs[v.ident]
+			for _, f in ipairs(struct.fields) do
+				table.insert(items, f)
+			end
+			for _, m in ipairs(struct.methods) do
+				table.insert(items, m)
+			end
+		else
+			table.insert(items, v)
+		end
+	end
+	for _, name in ipairs(self.pending) do
+		local struct = self.structs[name]
+		for _, m in ipairs(struct.methods) do
+			table.insert(items, m)
+		end
+	end
+	return items
+end
+
 local LocationList = {
 	indent = {
 		kind = {
@@ -122,14 +240,22 @@ local LocationList = {
 
 function LocationList.on_list(opts)
 	opts.title = "Document symbols"
-	opts.items = vim.iter(opts.items)
+	opts.items = LocationList.group(opts.items)
+	opts.quickfixtextfunc = LocationList.quickfixtextfunc
+	vim.fn.setloclist(0, {}, " ", opts)
+	vim.cmd.lopen()
+end
+
+function LocationList.group(items)
+	local lt = LocationTree:new()
+	for _, item in ipairs(items) do
+		lt:Add(item)
+	end
+	return vim.iter(lt:Items())
 		:map(function(o)
 			return LocationList.rename(o)
 		end)
 		:totable()
-	opts.quickfixtextfunc = LocationList.quickfixtextfunc
-	vim.fn.setloclist(0, {}, " ", opts)
-	vim.cmd.lopen()
 end
 
 -- rename replaces the "[Kind]" prefix in the location list items text with
