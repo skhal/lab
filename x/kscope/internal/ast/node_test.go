4ea24b65af9d4680a2a857252fe624a36905aea2
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-05-04 06:20:31 -0500

    x/kscope: add examples for AST node strings
    The examples show what a node output may look like.
    
    Issue #257

---
+++ x/kscope/internal/ast/node_test.go
@@ -0,0 +1,30 @@
+// Copyright 2026 Samvel Khalatyan. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package ast_test
+
+import (
+	"fmt"
+
+	"github.com/skhal/lab/x/kscope/internal/ast"
+)
+
+func ExampleNumber_String() {
+	var n ast.Node = ast.Number{Val: 1.23}
+	fmt.Println(n)
+	// Output:
+	// 1.2
+}
+
+func ExampleBinExpr_String() {
+	var n ast.Node = ast.BinExpr{
+		Op:    ast.BinOpPlus,
+		Left:  ast.Number{Val: 1},
+		Right: ast.Number{Val: 2},
+	}
+	fmt.Println(n)
+	// Output:
+	// 1.0 + 2.0
+}
