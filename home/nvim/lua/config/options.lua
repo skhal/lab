36565021cef086930bf084a68b81b96b65de2556
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-05-18 11:45:33 -0500

    nvim: clean options
    Re-order option groups: general, indentation, fixes.
    
    Close #290

--- home/nvim/lua/config/options.lua
+++ home/nvim/lua/config/options.lua
@@ -3,23 +3,29 @@
 -- Use of this source code is governed by a BSD-style
 -- license that can be found in the LICENSE file.
 
+local opt = vim.o
+
+-- General
 -- keep-sorted start
-vim.o.number = true
+opt.number = true
+opt.termguicolors = true
 -- keep-sorted end
+
+-- Indentation
+-- keep-sorted start
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.smartindent = true
+opt.softtabstop = 2
+opt.tabstop = 2
+-- keep-sorted end
+
+-- Invisibles
+opt.listchars = "eol:¬,extends:›,precedes:‹,space:░,tab:«–»,trail:•"
+vim.keymap.set({ "n" }, "<leader>l", "<esc>:set list!<cr>")
 
 -- fix +q4D73 shown at Neovim startup.
 -- Ref: https://github.com/neovim/neovim/issues/28776
 local termfeatures = vim.g.termfeatures or {}
 termfeatures.osc52 = false
 vim.g.termfeatures = termfeatures
-
-vim.o.listchars = "eol:¬,extends:›,precedes:‹,space:░,tab:«–»,trail:•"
-vim.keymap.set({ "n" }, "<leader>l", "<esc>:set list!<cr>")
-
--- keep-sorted start
-vim.o.expandtab = true
-vim.o.shiftwidth = 2
-vim.o.smartindent = true
-vim.o.softtabstop = 2
-vim.o.tabstop = 2
--- keep-sorted end
