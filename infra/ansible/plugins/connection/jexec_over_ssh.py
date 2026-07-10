12fb00f2e3abf6fac55ccfd23ad0ada64259ac91
Samvel Khalatyan <sn.khalatyan@gmail.com>
2026-07-10 17:16:17 -0500

    plugins/connetion: implement jail over ssh connection
    The new connection type, jail_over_ssh, uses SSH to connect to the
    remote host and run commands inside a running jail using jexec(8).
    
    It takes a single option, jail name.
    
    Tested with ping module:
    
        $ ansible dns.lab.net \
            -c jexec_over_ssh \
            -m ping \
            -e ansible_ssh_host=nuc.lab.net \
            -e ansible_jexec_over_ssh_jail_name=dns \
            --become --become-method=community.general.doas
    
    Issue #407

--- infra/ansible/plugins/connection/jexec_over_ssh.py
+++ infra/ansible/plugins/connection/jexec_over_ssh.py
@@ -3,19 +3,63 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-from typing import override
-import yaml
+import dataclasses
+import json
+import os
+from contextlib import contextmanager
+from dataclasses import dataclass
+from functools import wraps
+from typing import Callable, Optional, override
 
+import yaml
+from ansible.errors import AnsibleError
+from ansible.module_utils.common.text.converters import to_text
 from ansible.plugins.connection import ssh
 from ansible.plugins.connection.ssh import Connection as SSHConnection
-from ansible.utils.display import Display
 
 DOCUMENTATION = r"""
 name: jexec_over_ssh
 short_description: Run tasks using jexec on the remote host.
 description:
     - Run commands or put/fetch files to a jail on the remote host using jexec.
+options:
+    jail_name:
+        description:
+            - Jail name to connect to.
+        type: str
+        required: true
+        vars:
+            - name: ansible_jexec_over_ssh_jail_name
 author: Samvel Khalatyan
+"""
+
+EXAMPLES = r"""
+# SSH to bar.example.com and jexec to the jail foo, assuming the jail is
+# registered in the inventory under foo.example.com hostname.
+- name: Example
+  hosts: foo.example.com
+  connection: jexec_over_ssh
+  vars:
+    # ssh to bar.example.ecom
+    ansible_ssh_host: bar.example.com
+    # jexec to foo jail
+    ansible_jexec_over_ssh_jail_name: foo
+  gather_facts: false
+  # elevate privileges to run jexec.
+  become: true
+  become_method: community.general.doas
+  tasks:
+    - name: Ping the jail
+      ansible.builtin.ping:
+
+# The same but using Ansible CLI:
+# ansible foo.example.com \
+#   -b \
+#   --become-method=community.general.doas \
+#   -c jexec_over_ssh \
+#   -e ansible_jexec_over_ssh_jail_name=foo \
+#   -e ansible_ssh_host=bar.example.com \
+#   -m ping
 """
 
 _DOC_OPTIONS_KEY = "options"
@@ -34,24 +78,111 @@ def _merge_doc_options(src: str, dst: str) -> str:
 
 globals().update(DOCUMENTATION=_merge_doc_options(ssh.DOCUMENTATION, DOCUMENTATION))
 
-display = Display()
+# keep-sorted start
+_CP_CMD = "/bin/cp"
+_JEXEC_CMD = "/usr/sbin/jexec"
+_JLS_CMD = "/usr/sbin/jls"
+_MKTEMP_CMD = "/usr/bin/mktemp"
+_RM_CMD = "/bin/rm"
+# keep-sorted end
+
+
+@dataclass
+class Jail:
+    name: str
+    path: str
+
+
+class JailNotFoundError(AnsibleError):
+    """JailNotFoundError means specified jail is not found."""
+
+    def __init__(self, name: str):
+        super().__init__(f"jail {name} not found")
+
+
+def as_jail_list(d: dict):
+    """Remove wrapper json header and parse jails.
+
+    jls(8) JSON output is:
+        {
+          "__version": "2",
+          "jail-information": {
+            "jail": [
+              {
+                "jid": 123
+                "name": "foo",
+                "path": "/jail/foo"
+              }
+            ]
+          }
+        }
+
+    This function parses every jail as Jail structure and removes the wrapper
+    layer at "jail-information". The returned data are:
+
+        dict(jail=[
+            Jail(name="foo", path="/jail/foo"),
+        ])
+    """
+    jail_information_key = "jail-information"
+    if jail_information_key in d:
+        return d[jail_information_key]
+    if "jid" not in d:
+        return d
+    # jls output parameters must match Jail field names
+    keys = {f.name for f in dataclasses.fields(Jail)}
+    return Jail(**{k: d[k] for k in d.keys() & keys})
 
 
 class Connection(SSHConnection):
     """Remote BSD jail connection."""
 
+    _OPT_JAIL_NAME = "jail_name"
+
     transport = "jexec_over_ssh"  # plugin identifier
     has_pipelining = True  # use persistent connection
 
     def __init__(self, *args, **kwargs) -> None:
-        super(Connection, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
+        self._jail = None
+        self._remote_host = None
 
-    @override
-    def _connect(self) -> Connection:
-        """Establish a connection to the remote host."""
-        super(Connection, self)._connect()
-        display.vvv("JEXEC-OVER-SSH: connected", host=self._play_context.remote_addr)
-        return self
+    @property
+    def jail_name(self):
+        if not self._jail:
+            self._jail = self._init_jail()
+        return self._jail.name
+
+    @property
+    def jail_path(self):
+        if not self._jail:
+            self._jail = self._init_jail()
+        return self._jail.path
+
+    @property
+    def remote_host(self):
+        if not self._remote_host:
+            self._remote_host = (
+                self.get_option("host") or self._play_context.remote_addr
+            )
+        return self._remote_host
+
+    def _init_jail(self) -> Jail:
+        jname = self.get_option(Connection._OPT_JAIL_NAME)
+        cmd = [_JLS_CMD, "--libxo=json", "-j", jname, "jid", "name", "path"]
+        rc, out, _ = super().exec_command(" ".join(cmd))
+        if rc != os.EX_OK:
+            raise JailNotFoundError(jname)
+        return self._parse_jls_output(to_text(out.strip()), jname)
+
+    def _parse_jls_output(self, s: str, name: str) -> Jail:
+        jail_list = json.loads(s, object_hook=as_jail_list)
+        jails = {j.name: j for j in jail_list.get("jail", [])}
+        if name not in jails:
+            raise JailNotFoundError(name)
+        jail = jails[name]
+        self._display.vvv(f"{self.transport}: validated jail {jail}")
+        return jail
 
     @override
     def exec_command(
@@ -60,21 +191,73 @@ class Connection(SSHConnection):
         """Execute a remote command cmd with optional stdin in_data and
         escalated privileges when sudoable is set to True.
         """
-        return super(Connection, self).exec_command(
-            cmd, in_data=in_data, sudoable=sudoable
+        self._display.vvv(
+            f"{self.transport}: original command: {cmd}",
+            host=f"{self.remote_host} {self.jail_name}",
         )
+        jexec_cmd = [
+            _JEXEC_CMD,
+            "-l",  # run in clean environment
+            self.jail_name,
+            cmd,
+        ]
+        return self._exec_command(jexec_cmd, in_data, sudoable)
+
+    def _exec_command(
+        self, command: list[str], in_data=None, sudoable=True
+    ) -> tuple[int, bytes, bytes]:
+        cmd = " ".join(command)
+        if self.become:
+            cmd = self.become.build_become_command(cmd, self._shell)
+        self._display.vvv(
+            f"{self.transport}: {cmd}",
+            host=f"{self.remote_host} {self.jail_name}",
+        )
+        return super().exec_command(cmd, in_data=in_data, sudoable=sudoable)
 
     @override
     def put_file(self, in_path: str, out_path: str) -> tuple[int, bytes, bytes]:
         """Transfer a local in_path file to the remote out_path file."""
-        return super(Connection, self).put_file(in_path, out_path)
+        with self._remote_mktemp() as tmp_file:
+            rc, out, err = super().put_file(in_path, tmp_file)
+            if rc != os.EX_OK:
+                return rc, out, err
+            cp_cmd = [
+                _CP_CMD,
+                "-f",
+                "-p",  # preserve attributes
+                tmp_file,
+                os.path.join(self.jail_path, out_path),
+            ]
+            return self._exec_command(cp_cmd)
 
     @override
     def fetch_file(self, in_path: str, out_path: str) -> tuple[int, bytes, bytes]:
         """Fetch a remote in_path file to the local out_path file."""
-        return super(Connection, self).fetch_file(in_path, out_path)
+        with self._remote_mktemp() as tmp_file:
+            cp_cmd = [
+                _CP_CMD,
+                "-f",
+                "-p",  # preserve attributes
+                os.path.join(self.jail_path, in_path),
+                tmp_file,
+            ]
+            rc, out, err = self._exec_command(cp_cmd)
+            if rc != os.EX_OK:
+                return rc, out, err
+            return super().fetch_file(tmp_file, out_path)
 
-    @override
-    def close(self) -> None:
-        """Close the remote connection."""
-        super(Connection, self).close()
+    @contextmanager
+    def _remote_mktemp(self):
+        """Creates a temporary file on the remote host. It removes the file upon
+        exit from the context."""
+        cmd = [_MKTEMP_CMD, "--quiet"]
+        rc, out, _ = super().exec_command(" ".join(cmd))
+        if rc != os.EX_OK:
+            raise AnsibleError(f"failed to create temporary file on {self.remote_host}")
+        tmp_file = to_text(out.strip())
+        try:
+            yield tmp_file
+        finally:
+            cmd = [_RM_CMD, "-f", tmp_file]
+            super().exec_command(" ".join(cmd))
